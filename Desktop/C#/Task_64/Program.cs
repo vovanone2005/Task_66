368579238742469f5a25275189db389120e71d06
vovanone2005 <voevoda.junior@mail.ru>
2022-09-17 18:45:42 +0300

    Task_64

---
+++ Desktop/C#/Task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = InputNumbers("Введите a: ");
+int count = 2;
+PrintNumber(a, count);
+Console.Write(1);
+
+void PrintNumber(int a, int count)
+{
+  if (count > a) return;
+  PrintNumber(a, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
