c3631297867d87d946f5567eba29f600d18b70df
vovanone2005 <voevoda.junior@mail.ru>
2022-09-17 18:57:43 +0300

    Task_66

---
+++ Desktop/C#/Task_66/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = InputNumbers("Введите a: ");
+int b = InputNumbers("Введите b: ");
+int temp = a;
+
+if (a > b) 
+{
+  a = b; 
+  b = temp;
+}
+
+
+PrintSumm(a, b, temp=0);
+
+void PrintSumm(int a, int b, int summ)
+{
+  summ = summ + b;
+  if (b <= a)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(a, b - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
